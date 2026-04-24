7128eb00852fe1710adfee9f4841fbc132492ce2
Aleksandr Loktionov <loktionov129@gmail.com>
2026-04-24 14:46:22 +0700

    basic verstka

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -3,10 +3,32 @@ const container = document.getElementById("root");
 
 if (container) {
   const root = createRoot(container);
+  const options = [
+    "Reposts",
+    "Likes",
+    "Subscribers",
+    "Members",
+    "Comments",
+    "Votings"
+  ];
+
   root.render(
     <div className="p-6">
-      <h1 className="text-2xl">Hello, World</h1>
-      <p className="text-base">This options page is rendered with React</p>
+      <h1 className="text-2xl">friendly2</h1>
+
+      <h2 className="text-2xl">Likes.FM</h2>
+      <ul>
+        {options.map(option => (
+          <li className="flex items-center gap-2"> 
+            <label>
+              <input type="checkbox" />
+              &nbsp;
+              {option}
+            </label>
+          </li>
+        ))}
+      </ul>
+      
     </div>
   );
 }
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,8 +1,13 @@
 export function App() {
   return (
     <div className="w-80 p-6">
-      <h1 className="text-2xl">Hello, World</h1>
-      <p className="text-base">This popup page is rendered with React</p>
+      <h1 className="text-2xl">friendly 2</h1>
+      <p className="text-base">
+        <a href="https://likes.fm/" target="_blank" className="inline-flex items-center gap-2">
+          <img src="https://likes.fm/static/images/durov.ico" alt="likes.fm" className="w-5 h-5" />
+          Likes.FM
+        </a>
+      </p>
     </div>
   );
 }
